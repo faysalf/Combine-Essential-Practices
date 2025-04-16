21df0370c32b8178ebc04b102f17f06aa7b0fb5f
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2025-04-16 10:45:34 +0600

    129 Parameter passing

--- 28-128-CustomOperators.playground/Contents.swift
+++ 28-128-CustomOperators.playground/Contents.swift
@@ -6,7 +6,12 @@ import Combine
 extension Publisher where Output == Int {
     
     func fetchOddNumbers() -> AnyPublisher<Int, Failure> {
-        return self.filter { $0 & 1 == 1 }
+        return self.filter { $0&1 == 1 }
+            .eraseToAnyPublisher()
+    }
+    
+    func fetchGreater(than value: Int) -> AnyPublisher<Int, Failure> {
+        return self.filter { $0 > value }
             .eraseToAnyPublisher()
     }
     
@@ -16,6 +21,12 @@ extension Publisher where Output == Int {
 // Usage
 let arrPublisher = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].publisher
 let cancellable = arrPublisher.fetchOddNumbers()
+    .sink { value in
+        print(value)
+    }
+
+print("Greater than 5")
+let _ = arrPublisher.fetchGreater(than: 5)
     .sink { value in
         print(value)
     }
